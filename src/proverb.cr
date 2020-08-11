cbb504384bfb60e1086212385e5c1c06c789ba18
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-12 09:52:48 +1000

    feat(proverb.cr): added empty array option to recite method to pass first test in spec

--- src/proverb.cr
+++ src/proverb.cr
@@ -1,1 +1,9 @@
 # Please implement your solution to proverb in this file
+class Proverb
+    def self.recite(words_array : Array)
+        if words_array.empty?
+            return [] of String
+        end
+        # if words_array
+    end
+end
