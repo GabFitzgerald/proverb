aa81f782c3629c8a631c3d142d693c674080b2ca
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-12 10:35:41 +1000

    refactor(proverb.cr): renamed variables

--- src/proverb.cr
+++ src/proverb.cr
@@ -1,12 +1,12 @@
 # Please implement your solution to proverb in this file
 class Proverb
-    def self.recite(words_array : Array) : Array
-        proverbs_array = [] of String
+    def self.recite(words : Array) : Array
+        proverbs = [] of String
   
-        (0...words_array.size-1).each { |x| proverbs_array.push("For want of a #{words_array[x]} the #{words_array[x + 1]} was lost.") } if words_array.size > 1
+        (0...words.size-1).each { |x| proverbs.push("For want of a #{words[x]} the #{words[x + 1]} was lost.") } if words.size > 1
 
-        proverbs_array.push("And all for the want of a #{words_array[0]}.") if words_array.size > 0
+        proverbs.push("And all for the want of a #{words[0]}.") if words.size > 0
 
-        return proverbs_array     
+        return proverbs     
     end
 end
