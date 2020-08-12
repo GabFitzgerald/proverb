199ae3d0d0248a143b421efabd2a1394614c33ac
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-12 10:09:44 +1000

    feat(proverb.cr): written solution for proverb problem

--- src/proverb.cr
+++ src/proverb.cr
@@ -2,15 +2,19 @@
 class Proverb
     def self.recite(words_array : Array)
         proverbs_array = [] of String
-        # return [] of String if words_array.empty?
-        
-        if words_array.size == 1
+  
+        if words_array.size > 1
+            x = 0
+            while x < words_array.size - 1
+                proverbs_array.push("For want of a #{words_array[x]} the #{words_array[x + 1]} was lost.")
+                x +=1
+            end
+        end
+
+        if words_array.size > 0
             proverbs_array.push("And all for the want of a #{words_array[0]}.")
         end
 
         return proverbs_array     
     end
-
-    puts recite(["nail"])
-
 end
