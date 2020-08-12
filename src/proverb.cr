6d88ca225fde4ba3d6b7e0e5d557de0187350df2
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-12 10:33:28 +1000

    refactor: removed while-loop style programing
    changed while-loop style programming to .each method on a range of indexes in an array (I think
    that's a better way to do it)

--- src/proverb.cr
+++ src/proverb.cr
@@ -1,19 +1,11 @@
 # Please implement your solution to proverb in this file
 class Proverb
-    def self.recite(words_array : Array)
+    def self.recite(words_array : Array) : Array
         proverbs_array = [] of String
   
-        if words_array.size > 1
-            x = 0
-            while x < words_array.size - 1
-                proverbs_array.push("For want of a #{words_array[x]} the #{words_array[x + 1]} was lost.")
-                x +=1
-            end
-        end
+        (0...words_array.size-1).each { |x| proverbs_array.push("For want of a #{words_array[x]} the #{words_array[x + 1]} was lost.") } if words_array.size > 1
 
-        if words_array.size > 0
-            proverbs_array.push("And all for the want of a #{words_array[0]}.")
-        end
+        proverbs_array.push("And all for the want of a #{words_array[0]}.") if words_array.size > 0
 
         return proverbs_array     
     end
