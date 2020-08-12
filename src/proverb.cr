c2f14f7533bc72438afb0503316cc092c3a15e96
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-12 10:52:11 +1000

    refactor(proverb.cr): refactored to one line
    ty duke

--- src/proverb.cr
+++ src/proverb.cr
@@ -1,12 +1,14 @@
 # Please implement your solution to proverb in this file
 class Proverb
     def self.recite(words : Array) : Array
-        proverbs = [] of String
+        # proverbs = [] of String
   
-        (0...words.size-1).each { |x| proverbs.push("For want of a #{words[x]} the #{words[x + 1]} was lost.") } if words.size > 1
+        # (0...words.size-1).each { |x| proverbs.push("For want of a #{words[x]} the #{words[x + 1]} was lost.") } if words.size > 1
 
-        proverbs.push("And all for the want of a #{words[0]}.") if words.size > 0
+        # proverbs.push("And all for the want of a #{words[0]}.") if words.size > 0
 
-        return proverbs     
+        # return proverbs 
+        
+        words.each.map_with_index { |word, x| !(x == (words.size-1)) ? "For want of a #{words[x]} the #{words[x + 1]} was lost." : "And all for the want of a #{words[0]}." }
     end
 end
